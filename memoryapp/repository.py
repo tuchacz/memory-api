9c7d8d138bd62fa6360b43a2049fc7f62b36dd61
tuchacz <tuchacz@student.wszib.edu.pl>
2023-07-08 12:14:58 +0200

    Dodawanie kategori

--- memoryapp/repository.py
+++ memoryapp/repository.py
@@ -4,7 +4,21 @@ categories_list = [
     Category(1, 'Dom'),
     Category(2, 'Rodzina')
 ]
+id_categories = 2
 
 
 def get_categories():
     return categories_list
+
+
+def create_category(category_name):
+    category = Category(__next_category_id(), category_name)
+    categories_list.append(category)
+
+    return category
+
+def __next_category_id():
+    global id_categories
+    id_categories += 1
+    return id_categories
+
--- memoryapp/routes/categories.py
+++ memoryapp/routes/categories.py
@@ -1,10 +1,16 @@
-from flask import jsonify
+from flask import jsonify, request
 from memoryapp import app
 from memoryapp.repository import *
-
-
 
 
 @app.route('/categories', methods=['GET'])
 def categories():
     return jsonify(get_categories())
+
+
+@app.route('/categories', methods=['POST'])
+def add_category():
+    r = request.json
+    category_name = r['name']
+
+    return jsonify(create_category(category_name)), 201
