3bdaf0cbd8d87a289cb45deb24feece6caacaff6
tuchacz <tuchacz@student.wszib.edu.pl>
2023-07-08 10:39:38 +0200

    Konfiguracja i uruchomienie

---
+++ memoryapp/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)   # instancja clasy flask
+cors = CORS(app, resources={r"/*": {"origins": "*"}})    # wszystkie ścieżki które są w aplikacje zmniejszona jest restrykcja
+app.debug = True      # włącza aplikacje flask w trybie debugowania
+
+
---
+++ run.py
@@ -0,0 +1,4 @@
+from memoryapp import app
+
+if __name__ == '__main__':  # uruchomienie z poziomu tego pliku
+    app.run()
